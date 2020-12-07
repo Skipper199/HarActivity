1984641e68cb462383328c5dcb63cac81a1f9ac4
George Thomas <georgethomas806@gmail.com>
2020-12-07 19:38:38 +0200

    login-signup-frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useRouteMatch,
+  useHistory,
+} from "react-router-dom";
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Signup />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
