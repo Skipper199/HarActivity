b31dc1d1ee8769ad8661d73a8f6b6f40bdbde4fa
George Thomas <georgethomas806@gmail.com>
2020-12-08 12:20:55 +0200

    themes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import darkTheme from './themes/darkTheme';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+  <ThemeProvider theme={darkTheme}>
+    <Router>
+      <App />
+    </Router>
+  </ThemeProvider>,
+  document.getElementById('root')
 );
