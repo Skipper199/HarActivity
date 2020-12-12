00fff8236ba0dfae23228934a277cbf0bb50f2a6
Leo Tsant <leo.tsa@hotmail.com>
2020-12-12 23:15:00 +0200

    Profile and UploadFiles styling

--- client/src/components/User/Profile.js
+++ client/src/components/User/Profile.js
@@ -1,10 +1,210 @@
 import React from 'react';
-import Button from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Table from '@material-ui/core/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
+  appBar: {
+    backgroundColor: '#333333',
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  menuButtonHidden: {
+    display: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawerPaper: {
+    backgroundColor: '#373737',
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9),
+    },
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+    backgroundColor: '#212121',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    height: 250,
+  },
+}));
 
 const Profile = () => {
+  const classes = useStyles();
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   return (
     <div>
-      <Button variant="contained">Profile</Button>
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3} direction="column">
+          {/* Profile Settings */}
+          <Box marginLeft={2}>
+            <Typography component="p" variant="h4">
+              Profile Settings
+            </Typography>
+          </Box>
+          <Grid item xs={12} md={8} lg={7}>
+            <Paper className={fixedHeightPaper}>
+              <Box paddingLeft={1}>
+                <Typography component="p" variant="h5">
+                  Change Username
+                </Typography>
+              </Box>
+              <Grid container alignItems="center">
+                <Box mt={1} mr={0} mb={1} ml={1}>
+                  <TextField
+                    variant="outlined"
+                    style={{ width: 175 }}
+                    size="small"
+                    color="primary"
+                    name="usernameChange"
+                    label="New Username"
+                    id="usernameChange"
+                  />
+                </Box>
+                <Box padding={1}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    style={{ width: 75, height: 30 }}
+                  >
+                    Update
+                  </Button>
+                </Box>
+              </Grid>
+              <Box paddingTop={3} paddingLeft={1}>
+                <Typography component="p" variant="h5">
+                  Change Password
+                </Typography>
+              </Box>
+              <Grid container alignItems="center">
+                <Box mt={1} mr={1.5} mb={1} ml={1}>
+                  <TextField
+                    type="password"
+                    variant="outlined"
+                    style={{ width: 175 }}
+                    size="small"
+                    color="primary"
+                    name="oldPassword"
+                    label="Old Password"
+                    id="oldPassword"
+                  />
+                </Box>
+                <Box mt={1} mr={0} mb={1} ml={1}>
+                  <TextField
+                    type="password"
+                    variant="outlined"
+                    style={{ width: 175 }}
+                    size="small"
+                    color="primary"
+                    name="newPassword"
+                    label="Password"
+                    id="newPassword"
+                  />
+                </Box>
+                <Box padding={1}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    size="small"
+                    color="primary"
+                    style={{ width: 75, height: 30 }}
+                  >
+                    Update
+                  </Button>
+                </Box>
+              </Grid>
+            </Paper>
+          </Grid>
+          {/* Recent Uploaded Files */}
+          <Box marginTop={4} marginLeft={2}>
+            <Typography component="p" variant="h4">
+              Uploaded Files
+            </Typography>
+          </Box>
+          <Grid item xs={12} md={8} lg={7}>
+            <Paper className={fixedHeightPaper}>
+              <Table size="small">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Date Uploaded</TableCell>
+                    <TableCell>Size</TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
--- client/src/components/User/UploadFiles.js
+++ client/src/components/User/UploadFiles.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import Button from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import PublishIcon from '@material-ui/icons/Publish';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: 'none',
+  },
+}));
 
 const UploadFiles = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <Button variant="contained">Upload</Button>
+    <div className={classes.root}>
+      <input
+        accept=".har"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+      />
+      <Box padding={5} textAlign="center">
+        <Typography component="p" variant="h3">
+          Upload your HAR file
+        </Typography>
+      </Box>
+      <Box textAlign="center">
+        <label htmlFor="contained-button-file">
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ width: 250, height: 70 }}
+            component="span"
+          >
+            <PublishIcon />
+            <Box m={2}>
+              <h2>Upload File</h2>
+            </Box>
+          </Button>
+        </label>
+      </Box>
     </div>
   );
 };
